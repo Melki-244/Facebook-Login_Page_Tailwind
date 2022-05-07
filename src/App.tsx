6f2d7725204b50f0bfa8482c4e74e1a01264774d
Melki-244 <melkicavalcante244@gmail.com>
2022-05-07 11:55:05 -0300

    Criando e Limpando Projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="2xl font-bold ml-10">Teste</h1> 
+    </div>
+  );
+}
+
+export default App;
